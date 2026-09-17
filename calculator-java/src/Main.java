6d7924ef47539e5aecead8e2dd0890d5acf271b4
devshchetnikov <dmitrijsetnikov87@gmail.com>
2026-09-17 13:53:59 +0500

    fix: change submodule to normal folder

---
+++ calculator-java/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("\nВведите первое число или напишите (exit): ");
+            String input1 = scanner.next();
+
+            if (input1.equalsIgnoreCase("exit")) {
+                System.out.println("Программа завершена. До свидания!");
+                break;
+            }
+
+            int num1;
+            try {
+                num1 = Integer.parseInt(input1);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка! Введите корректное число или 'exit'.");
+                continue;
+            }
+
+            System.out.print("Введите второе число: ");
+            int num2 = scanner.nextInt();
+
+            System.out.print("Выберите действие (+, -, *, /): ");
+            String op = scanner.next();
+
+            int res;
+
+            switch (op) {
+                case "+":
+                    res = num1 + num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    System.out.print("Результаat: " + res);
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.print("Ошибка! Делить на ноль нельзя.");
+                    } else {
+                        res = num1 / num2;
+                        System.out.print("Результат: " + res);
+                    }
+                    break;
+                default:
+                    System.out.print("Неверная операция!");
+                    break;
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
