4ccd65cf3e71d6b9fb61137c559d83d5591131c7
Devshchetnikov <dmitrijsetnikov87@gmail.com>
2026-09-25 19:28:27 +0500

    feat: complete calculator formatting and add ans feature

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 import java.util.NoSuchElementException;
 
+
 public class Main {
 
     private static double ans = 0; // Переменная для хранения последнего результата
